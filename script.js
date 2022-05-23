0a06535292621c429394a6cbf44caa29b07f5071
medirudiantoni <92556546+medirudiantoni@users.noreply.github.com>
2022-05-23 21:33:58 +0700

    update err

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ const menus = document.querySelector('.menus');
 const general = document.querySelector('.general');
 const toggle = document.querySelector('.toggle');
 const close = document.querySelector('.close');
-const bgDark = document.querySelector('.switch-dark');
-const bgLight = document.querySelector('.switch-light');
-const body = document.getElementsByTagName('body');
-
-bgDark.addEventListener('click', function(){
-    body.setAttribute.class('bg-dark');
-})
 
 const slider = () => {
     menus.classList.toggle('slide');
